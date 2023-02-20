d1baf388ae37af6949968bcb58ed2ea0ee691fa8
dive2Pro <toylucklebo@gmail.com>
2023-02-20 15:10:17 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function run(): Promise<void> {
       reviewers: utils.getInputAsArray('reviewers'),
       teamReviewers: utils.getInputAsArray('team-reviewers'),
       milestone: Number(core.getInput('milestone')),
-      draft: core.getBooleanInput('draft')
+      draft: core.getBooleanInput('draft'),
+      dest: core.getInput('dest'),
     }
     core.debug(`Inputs: ${inspect(inputs)}`)
 
